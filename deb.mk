fa6511db43533e362d9313731be483ca488f2569
Pip Cet <pipcet@gmail.com>
2022-07-25 11:21:54 +0000

    deb.mk: try oldest kernel

--- deb.mk
+++ deb.mk
@@ -6,7 +6,7 @@ $(BUILD)/debian/deb/%.deb: $(BUILD)/debian/deb/Packages dg/deb.pl | $(BUILD)/deb
 	curl http://http.us.debian.org/debian/$(shell perl dg/deb.pl "$*" < $<) > $@
 
 $(BUILD)/debian/deb/linux-image.deb: $(BUILD)/debian/deb/Packages dg/deb.pl | $(BUILD)/debian/deb/
-	FILE=$$(egrep '^Package: linux-image-(.*)-cloud-arm64-unsigned' < $< | tail -1 | while read DUMMY PACKAGE; do echo $$PACKAGE; done).deb; $(MAKE) -f dg/dg.mk $(BUILD)/debian/deb/$$FILE; ln -sf $$FILE $@
+	FILE=$$(egrep '^Package: linux-image-(.*)-cloud-arm64-unsigned' < $< | head -1 | while read DUMMY PACKAGE; do echo $$PACKAGE; done).deb; $(MAKE) -f dg/dg.mk $(BUILD)/debian/deb/$$FILE; ln -sf $$FILE $@
 
 $(BUILD)/qemu-kernel: $(BUILD)/debian/deb/linux-image.deb
 	$(MKDIR) $(BUILD)/kernel
